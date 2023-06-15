0416e068f67a744749de503d667a35b793f9ae7e
Sergey Salakhov <netbeholder@gmail.com>
2023-06-15 15:22:11 +0300

    Initial setting for Virtual Machine

---
+++ voidlinux-install-scripts/09-vm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# use doas if installed
+
+# Set Privilege-Escalation-Command OR presco OR pr-es-co
+
+[ -x "$(command -v doas)" ] && [ -e /etc/doas.conf ] && presco="doas"
+[ -x "$(command -v sudo)" ] && presco="sudo"
+
+#Install open-vm-tools (vmtoolsd)
+
+$presco xbps-install -Suy open-vm-tools
+$presco cp /etc/vmware-tools/tools.conf.example /etc/vmware-tools/tools.conf
+
+#autochange screen resolution
+$presco sed '/\[resolutionKMS\]/a enable=true' /etc/vmware-tools/tools.conf
+
+$presco ln -s /etc/sv/vmtoolsd/ /var/service
+
+#$presco xbps-install -Suy xf86-video-vmware xorg xterm
