76d196fa1926806b95c87d026595f567a610831e
Sergey Salakhov <netbeholder@gmail.com>
2023-06-18 20:04:07 +0300

    Add script for creating the new user in fresh system

---
+++ voidlinux-install-scripts/20-create_new_user.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# use doas if installed
+
+# Set Privilege-Escalation-Command OR presco OR pr-es-co
+
+[ -x "$(command -v doas)" ] && [ -e /etc/doas.conf ] && presco="doas"
+[ -x "$(command -v sudo)" ] && presco="sudo"
+
+UserName="sergey"
+UserPassword="P@ssw0rd"
+
+#some xdg's software and doas
+$presco xbps-install -Suy xdg-utils xdg-user-dirs opendoas
+
+#create user with temporary password
+$presco useradd -s /bin/bash -m -G wheel,users,video,audio,lp,storage,scanner,input,socklog $UserName -p $UserPassword
+
+#create xdg compliant dirs in ~
+sudo -i -u $UserName xdg-user-dirs-update
+
+#simple doas.conf for created user
+$presco cat <<EOF > /etc/doas.conf
+permit persist sergey as root
+permit setenv { XAUTHORITY LANG LC_ALL } $UserName
+permit nopass $UserName as root cmd xbps-install
+permit nopass $UserName as root cmd updatedb
+permit nopass $UserName as root cmd nano
+EOF
+
+#set user's pass finally
+echo "Set password for new user:"
+passwd $UserName
