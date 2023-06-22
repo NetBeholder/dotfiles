885c1e0d47343b9431c2f6b1476b5f1d3d6b6d63
Sergey Salakhov <netbeholder@gmail.com>
2023-06-22 12:15:30 +0300

    stow: add git config

--- voidlinux-install-scripts/21-install_users_software.sh
+++ voidlinux-install-scripts/21-install_users_software.sh
@@ -6,9 +6,8 @@ set -euo pipefail
 # use doas if installed
 # Set Privilege-Escalation-Command OR presco OR pr-es-co
 
-[ -x "$(command -v doas)" ] && [ -e /etc/doas.conf ] && presco="doas"
 [ -x "$(command -v sudo)" ] && presco="sudo"
-
+[ -x "$(command -v doas)" ] && [ -e /etc/doas.conf ] && presco="doas"
 
 # $HOME folder structure:
 echo -e "
@@ -24,6 +23,8 @@ $HOME/.local/src/suckless
 $HOME/.local/src/suckless/tarfiles
 " | xargs mkdir -p
 
+$presco xbps-install -Suy stow 
+
 #Stow
 _path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo ".")")" && pwd)"
 cd "$_path/../stow" #~/.dotfiles/stow
@@ -33,14 +34,18 @@ pwd
 ##bash
 
 [ -f $HOME/.bash_aliases ] && { mv $HOME/.bash_aliases $HOME/.local/share/.old-stow-files; } || echo "bash_alias does not exist."
-
 [ -f $HOME/.bash_profile ] && { mv $HOME/.bash_profile $HOME/.local/share/.old-stow-files; } || echo "bash_profile does not exist."
-
 [ -f $HOME/..bashrc ] && { mv $HOME/.bashrc $HOME/.local/share/.old-stow-files; } || echo "bashrc does not exist."
 
 #mv ~/.bash_profile ~/.local/share/.old-stow-files/
 #mv ~/.bashrc ~/.local/share/.old-stow-files/
 
 #stow -t ~/ bash --dotfiles --adopt
+
+#git
+[ -f $HOME/.gitconfig ] && { mv $HOME/.gitconfig $HOME/.local/share/.old-stow-files; } || echo ".gitconfig does not exist."
+stow -t ~/ git --dotfiles #--adopt
+
+
 stow -t ~/ * --dotfiles --adopt
 $presco xbps-install -Suy stow nano vim alacritty #stow and software
